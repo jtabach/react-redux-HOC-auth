91b98c1425199f54b8bc7d241a0b7534d22af908
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-24 23:05:07 -0700

    checks user authentication on mount and update

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
-      console.log(this.props.authenticated);
       return <ComposedComponent {...this.props} />
     }
   }
