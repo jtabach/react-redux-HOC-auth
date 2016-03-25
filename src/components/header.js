f2a59db58ac4c35513f99cfb4ee539b9cd74206d
jtabach <jeff.n.tabachnick@gmail.com>
2016-03-24 21:47:17 -0700

    adds authentication action and renders according to sign in or out

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Header extends Component {
   authButton() {
+    if (this.props.authenticated) {
+      return (
+        <button onClick={() => this.props.authenticate(false)}>Sign Out</button>
+      );
+    }
+
     return (
-      <button>Sign In</button>
-    )
+      <button onClick={() => this.props.authenticate(true)}>Sign In</button>
+    );
   }
 
   render() {
@@ -27,4 +35,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.authenticated }
+}
+
+export default connect(mapStateToProps, actions)(Header);
